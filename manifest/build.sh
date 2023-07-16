00533aaa1ec93b947b7432afdf4014629835422f
Phoenix / Hotaru <DeriousHD@gmail.com>
2023-07-16 14:01:05 +0100

    [Dockerhub] Username change

--- manifest/build.sh
+++ manifest/build.sh
@@ -4,11 +4,11 @@
 . ./version.txt
 
 # Build panel
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$PANEL_VERSION -t mrflutters/pterodactyl-panel:$PANEL_VERSION ./panel
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$PANEL_VERSION -t hotarublaze/pterodactyl-panel:$PANEL_VERSION ./panel
 
 # Build Daemon
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$DAEMON_VERSION -t mrflutters/pterodactyl-daemon:$DAEMON_VERSION ./daemon
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$DAEMON_VERSION -t hotarublaze/pterodactyl-daemon:$DAEMON_VERSION ./daemon
 
 # Build Dedicated SFTP Server
-DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$SFTP_VERSION -t mrflutters/pterodactyl-sftp:$SFTP_VERSION ./sftp
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=$SFTP_VERSION -t hotarublaze/pterodactyl-sftp:$SFTP_VERSION ./sftp
 
